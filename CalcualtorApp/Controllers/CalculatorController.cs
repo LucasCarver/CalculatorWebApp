7f3dbca70147bee5a9c1d2054cd3a5a6c7e1311c
LucasCarver <carver.lucas@gmail.com>
2020-05-19 14:24:17 -0400

    First calculator webapp commit

---
+++ CalcualtorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalcualtorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalcualtorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(Calculator calculator)
+        {
+            if(calculator.Operator == '+')
+            {
+                calculator.Result = calculator.Number1 + calculator.Number2;
+            }
+            else if(calculator.Operator == '-')
+            {
+                calculator.Result = calculator.Number1 - calculator.Number2;
+            }
+            else if(calculator.Operator == '*')
+            {
+                calculator.Result = calculator.Number1 * calculator.Number2;
+            }
+            else if(calculator.Operator == '/')
+            {
+                calculator.Result = calculator.Number1 / calculator.Number2;
+            }
+
+            return View("Calculator", calculator);
+        }
+    }
+}
---
+++ CalcualtorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalcualtorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
